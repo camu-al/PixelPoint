ffd44a09b92eff6278a49e531f59979ed9c16cab
SEC-2006 <76954735+SEC-2006@users.noreply.github.com>
2025-05-12 08:50:37 +0200

    Update juegos.php top juegos

--- htdocs infinityfree/juegos.php
+++ htdocs infinityfree/juegos.php
@@ -7,7 +7,7 @@
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <p>Editado</p>
+    <h1>Top juegos</h1>
     <table class="table table-bordered">
         <thead>
             <th>Juego</th>
